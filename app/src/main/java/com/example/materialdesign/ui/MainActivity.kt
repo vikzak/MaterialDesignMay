e63fb64502da053ea60b677f97db3367f3bd6b34
vikzak <git@azbukait.ru>
2022-05-29 02:35:23 +0300

    10) темы переключаются на фрагменте настроек и сохраняются в SP

--- app/src/main/java/com/example/materialdesign/ui/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/ui/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.materialdesign.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.PersistableBundle
-import com.example.materialdesign.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.materialdesign.R
+import com.example.materialdesign.databinding.ActivityMainBinding
+
+
+const val DefaultThemeIndex = 1
+const val OrangeThemeIndex = 2
+const val GreenThemeIndex = 3
+const val KEY_SP = "sp"
+const val KEY_CURRENT_THEME = "current_theme"
+
+class MainActivity : AppCompatActivity(com.example.materialdesign.R.layout.activity_main) {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // задаем тему
-        setTheme(R.style.MyDefaultTheme)
-        setContentView(R.layout.activity_main)
+        setTheme(getRealStyleStyle(getCurrentTheme()))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(com.example.materialdesign.R.layout.activity_main)
+        setContentView(binding.root)
         if (savedInstanceState==null){
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container_view,MainFragment.newInstance())
+                .replace(com.example.materialdesign.R.id.fragment_container_view,MainFragment.newInstance())
                 .commit()
+        }
+    }
+
+
+    private fun getCurrentTheme(): Any {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME, -1)
+    }
+
+    private fun getRealStyleStyle(currentTheme: Any): Int {
+        return when (currentTheme) {
+            DefaultThemeIndex -> com.example.materialdesign.R.style.MyDefaultTheme
+            OrangeThemeIndex -> com.example.materialdesign.R.style.MyOrangeTheme
+            GreenThemeIndex -> com.example.materialdesign.R.style.MyGreenTheme
+            else -> 0
         }
     }
 
--- app/src/main/java/com/example/materialdesign/ui/SettingFragment.kt
+++ app/src/main/java/com/example/materialdesign/ui/SettingFragment.kt
@@ -5,13 +5,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import com.example.materialdesign.R
 import com.example.materialdesign.databinding.FragmentMainBinding
 import com.example.materialdesign.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment() {
+class SettingFragment : Fragment(), View.OnClickListener {
+    //    private val KEY_SP = "sp"
+//    private val KEY_CURRENT_THEME = "current_theme"
+    // для изменений в локальной теме
+    private val KEY_SP_LOCAL = "sp_local"
+    private val KEY_CURRENT_THEME_LOCAL = "current_theme_local"
+
+    private lateinit var parentActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = activity as MainActivity // воторой способ
+    }
+
     private var _binding: FragmentSettingBinding? = null
     private val binding: FragmentSettingBinding
         get() = _binding!!
@@ -20,21 +35,18 @@ class SettingFragment : Fragment() {
         fun newInstance() = SettingFragment()
     }
 
-    private lateinit var parentActivity: MainActivity
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        parentActivity = activity as MainActivity // воторой способ
-        parentActivity =
-            requireActivity() as MainActivity
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        val context: Context =
+            ContextThemeWrapper(activity, getRealStyleLocal(getCurrentThemeLocal()))
+        val localInflater = inflater.cloneInContext(context)
+        //_binding = FragmentSettingBinding.inflate(inflater, container, false)
+        //_binding = FragmentSettingBinding.inflate(inflater)
+        _binding = FragmentSettingBinding.inflate(localInflater)
         return binding.root
     }
 
@@ -51,11 +63,64 @@ class SettingFragment : Fragment() {
             }
         }
 
-
+        binding.themeDefault.setOnClickListener(this)
+        binding.themeOrange.setOnClickListener(this)
+        binding.themeGreen.setOnClickListener(this)
+        when (getCurrentThemeLocal()) {
+            1 -> binding.themeDefault.isChecked = true
+            2 -> binding.themeOrange.isChecked = true
+            3 -> binding.themeGreen.isChecked = true
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onClick(p0: View) {
+        when (p0.id) {
+            R.id.theme_default -> {
+                setCurrentThemeLocal(DefaultThemeIndex)
+                parentActivity.setTheme(R.style.MyDefaultTheme)
+                parentActivity.recreate()
+            }
+            R.id.theme_orange -> {
+                setCurrentThemeLocal(OrangeThemeIndex)
+                parentActivity.setTheme(R.style.MyOrangeTheme)
+                parentActivity.recreate()
+            }
+            R.id.theme_green -> {
+                setCurrentThemeLocal(GreenThemeIndex)
+                parentActivity.setTheme(R.style.MyGreenTheme)
+                parentActivity.recreate()
+            }
+        }
+    }
+
+    private fun setCurrentThemeLocal(currentTheme: Int) {
+//        val sharedPreferences = // для темы фрагмента
+//            requireActivity().getSharedPreferences(KEY_SP_LOCAL, AppCompatActivity.MODE_PRIVATE)
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(KEY_SP, AppCompatActivity.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        //editor.putInt(KEY_CURRENT_THEME_LOCAL, currentTheme) // для темы фрагмента
+        editor.putInt(KEY_CURRENT_THEME, currentTheme)
+        editor.apply()
+    }
+
+    private fun getCurrentThemeLocal(): Int {
+        val sharedPreferences = //здесь тоже можно указать локлаьные данные для темы фрагмента
+            requireActivity().getSharedPreferences(KEY_SP, AppCompatActivity.MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME, -1)
+    }
+
+    private fun getRealStyleLocal(currentTheme: Int): Int {
+        return when (currentTheme) {
+            DefaultThemeIndex -> R.style.MyDefaultTheme
+            OrangeThemeIndex -> R.style.MyOrangeTheme
+            GreenThemeIndex -> R.style.MyGreenTheme
+            else -> 0
+        }
     }
 }
