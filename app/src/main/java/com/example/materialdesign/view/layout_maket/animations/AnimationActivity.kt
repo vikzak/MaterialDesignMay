6b229e34e6fed3a1ae2589c79a7fdf5c46121cb1
vikzak <git@azbukait.ru>
2022-06-08 13:28:16 +0300

    ДЗ№5 4) Внес изменения в activity animation layout добавив еще одну кнопку для анимации перемещения кнопки из угла в угол по траектории через setPathMotion.

--- app/src/main/java/com/example/materialdesign/view/layout_maket/animations/AnimationActivity.kt
+++ app/src/main/java/com/example/materialdesign/view/layout_maket/animations/AnimationActivity.kt
@@ -2,19 +2,18 @@ package com.example.materialdesign.view.layout_maket.animations
 
 import android.os.Bundle
 import android.os.Handler
-import androidx.transition.TransitionManager
+import android.view.Gravity
 import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.postDelayed
-import androidx.transition.ChangeBounds
-import androidx.transition.Fade
-import androidx.transition.Slide
-import androidx.transition.TransitionSet
+import androidx.transition.*
 import com.example.materialdesign.databinding.ActivityAnimationBinding
 
 class AnimationActivity : AppCompatActivity() {
     lateinit var binding: ActivityAnimationBinding
     var animationFlag = false
+    var animationFlagPath = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,29 +40,41 @@ class AnimationActivity : AppCompatActivity() {
             binding.textViexActwAnimation.visibility =
                 if (animationFlag) View.VISIBLE else View.GONE
         }
+
+        val changeBounds = ChangeBounds()
+        changeBounds.duration = 3000
+        changeBounds.setPathMotion(ArcMotion())
+
+        binding.bottomAcbAnimationPath.setOnClickListener {
+            TransitionManager.beginDelayedTransition(binding.root, changeBounds)
+            animationFlagPath=!animationFlagPath
+            val params = binding.bottomAcbAnimationPath.layoutParams as FrameLayout.LayoutParams
+            params.gravity = if (animationFlagPath) Gravity.BOTTOM or  Gravity.END else Gravity.TOP or Gravity.START
+            binding.bottomAcbAnimationPath.layoutParams = params
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        Handler(mainLooper).postDelayed({
-            val transition = TransitionSet()
-            val fade = Fade()
-            val slide = Slide()
-
-            val changeBounds = ChangeBounds()
-            animationFlag = !animationFlag
-            //fade.duration = 5000
-            slide.duration = 3000
-            changeBounds.duration = 1000
-
-            //transition.ordering =TransitionSet.ORDERING_SEQUENTIAL
-            transition.ordering =TransitionSet.ORDERING_TOGETHER
-            //transition.addTransition(fade)
-            transition.addTransition(slide)
-            transition.addTransition(changeBounds)
-            TransitionManager.beginDelayedTransition(binding.animationContainer,transition)
-            binding.textViexActwAnimation.visibility =
-                if (animationFlag) View.VISIBLE else View.GONE
-        }, 1000)
+//        Handler(mainLooper).postDelayed({
+//            val transition = TransitionSet()
+//            val fade = Fade()
+//            val slide = Slide()
+//
+//            val changeBounds = ChangeBounds()
+//            animationFlag = !animationFlag
+//            //fade.duration = 5000
+//            slide.duration = 3000
+//            changeBounds.duration = 1000
+//
+//            //transition.ordering =TransitionSet.ORDERING_SEQUENTIAL
+//            transition.ordering =TransitionSet.ORDERING_TOGETHER
+//            //transition.addTransition(fade)
+//            transition.addTransition(slide)
+//            transition.addTransition(changeBounds)
+//            TransitionManager.beginDelayedTransition(binding.animationContainer,transition)
+//            binding.textViexActwAnimation.visibility =
+//                if (animationFlag) View.VISIBLE else View.GONE
+//        }, 1000)
     }
 }
