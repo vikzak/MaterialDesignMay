c0280134b39843fd3e85d9bb021422c761f4cfc3
vikzak <git@azbukait.ru>
2022-06-16 15:47:24 +0300

    ДЗ№7 1) добавлен в папку asset (подпапка font) шрифт ofont.ru_Daray.ttf. Он азгружается и применяется в фрагменте FaragmentDialogSheetBottom к textExplanationDialogSheetBottom.  Данный фрагмент можно увидеть нажав на кнопку слева в поле поиска в википедии.

--- app/src/main/java/com/example/materialdesign/view/ui/ESIBottomSheetDialogFragment.kt
+++ app/src/main/java/com/example/materialdesign/view/ui/ESIBottomSheetDialogFragment.kt
@@ -1,19 +1,38 @@
 package com.example.materialdesign.view.ui
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.materialdesign.R
+import com.example.materialdesign.databinding.FaragmentDialogSheetBottomBinding
+import com.example.materialdesign.databinding.FragmentMainBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ESIBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    private var _binding: FaragmentDialogSheetBottomBinding? = null
+    private val binding: FaragmentDialogSheetBottomBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.faragment_dialog_sheet_bottom, container, false)
+        _binding = FaragmentDialogSheetBottomBinding.inflate(inflater, container, false)
+        return binding.root
+        //return inflater.inflate(R.layout.faragment_dialog_sheet_bottom, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.textExplanationDialogSheetBottom.typeface =
+            Typeface.createFromAsset(requireContext().assets, "font/ofont.ru_Daray.ttf")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
